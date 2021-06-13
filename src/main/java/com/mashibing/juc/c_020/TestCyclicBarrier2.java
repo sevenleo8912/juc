a7bab1a2a6a1fe730fde4212c3de1dd952cf38a8
sevenleo8912 <xiaok_liu@163.com>
2021-06-13 19:42:06 +0800

    修改

--- src/main/java/com/mashibing/juc/c_020/TestCyclicBarrier2.java
+++ src/main/java/com/mashibing/juc/c_020/TestCyclicBarrier2.java
@@ -7,14 +7,14 @@ public class TestCyclicBarrier2 {
     public static void main(String[] args) {
 //        CyclicBarrier barrier = new CyclicBarrier(20);
 
-//        CyclicBarrier barrier = new CyclicBarrier(20, () -> System.out.println("满人"));
+        CyclicBarrier barrier = new CyclicBarrier(20, () -> System.out.println("满人"));
 
-        CyclicBarrier barrier = new CyclicBarrier(20, new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("满人，发车");
-            }
-        });
+//        CyclicBarrier barrier = new CyclicBarrier(20, new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("满人，发车");
+//            }
+//        });
 
         for(int i=0; i<100; i++) {
             new Thread(() -> {
